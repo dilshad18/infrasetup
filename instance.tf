0f0a0a2c16b0d182a5c64fde559d6be5cb664c87
root <root@ip-172-31-4-139.ap-south-1.compute.internal>
2018-11-19 12:04:24 +0000

    python in private box

--- instance.tf
+++ instance.tf
@@ -1,25 +1,20 @@
 resource "aws_instance" "public" {
   
-     ami             = "ami-0ff8a91507f77f867"
+     ami             = "ami-0ac019f4fcb7cb7e6"
      instance_type   = "t2.micro"
      subnet_id       = "${aws_subnet.public_subnet.id}"
      security_groups = ["${aws_security_group.security_group.id}"]     
      key_name        = "rootkey"
      user_data   = <<-EOF
 		   #!/bin/bash
-		  sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-                  sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-                  sudo yum install -y java-1.8.0-openjdk.x86_64
-                  sudo /usr/sbin/alternatives --set java /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java
-                  sudo /usr/sbin/alternatives --set javac /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/javac
-	          sudo yum install -y jenkins
-                  sudo service jenkins start
-                  sudo yum-config-manager --enable epel
-                  sudo yum install -y ansible
-                  sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
-                  sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-                  sudo yum install -y apache-maven
-		  EOF
+                  sudo add-apt-repository ppa:webupd8team/java -y
+                  sudo apt-get update && sudo add-apt-repository ppa:webupd8team/java -y
+                  wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+                  sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+                  sudo apt install  software-properties-common -y
+                  sudo apt-add-repository ppa:ansible/ansible -y
+                  sudo apt update && sudo apt install openjdk-8-jdk ansible jenkins -y
+		   EOF
      tags {
          Name        = "public_box"
         }
@@ -33,6 +28,11 @@ resource "aws_instance" "private" {
 	instance_type   = "t2.micro"
 	subnet_id       = "${aws_subnet.private_subnet.id}"
 	security_groups = ["${aws_security_group.security_group.id}"]
+	user_data       = <<EOF
+			  #!/bin/bash
+			  sudo apt update
+			  sudo apt install python -y
+			  EOF
 	tags {
 	 Name           = "private_box"
 	 }
